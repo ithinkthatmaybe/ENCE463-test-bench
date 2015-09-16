2a7a370857369710eb30b02ccd3981dbec5cb12c
martin <mcs108@uclive.ac.nz>
2015-09-16 12:10:20 +1200

    added more comments to code. added an explaination in reademe file.

--- PC_code/deliverable3.py
+++ PC_code/deliverable3.py
@@ -1,22 +1,43 @@
-from load_test import load_test
-import os
+# Python 2.7 code to load binary files on to the Stellaris
+# using "LM Flash Programmer". This code assumes that the
+# programmer is located in "C:\LMFlash.exe". You can 
+# download it from the TI website.
+#
+# Author: Martin Steinke 2015.
 
-testThing = load_test()
 
-#testThing.loadTest("P:/ENCE463/testBins/test1.bin")
+
+# import the class that interfaces with the programmer.
+from load_test import load_test 
+import os # For navigating the system.
+
+# Make an object to represent the Stellaris.
+ESTR = load_test()
+
+# Display some instructions
 print "-------------------------------------------------------------\n"
 print "Welcome to Group 06's basic UUT test manger!\n"
 print "Enter 1 for UART Mirror test, 2 for GPIO test or q to quit.\n"
 print "-------------------------------------------------------------\n"
 
+# Loop around; asking for input from the user.
 while True:
+	# Get user input.
 	test_num = raw_input("Enter test number: ")
-	if test_num == "q":
+	
+	# Check input to see if user wants to quit.
+	if test_num == "q": 
 		break
+	# Check if input is valid.
 	elif test_num not in ["1", "2", "q"]:
 		print "Invalid Entry!"
+	# Load the test onto Stellaris if input is valid.
 	else:
 		_path = os.getcwd() + "/testBins/test{}.bin".format(test_num)
-		testThing.loadTest(_path)
+		ESTR.loadTest(_path)
 	print "\n"
+
+# Done	
 print "That's all folks!"
+
+
