23c5548a63b25a0db01983b6a8a3fd5a299d8d81
unknown <bel40@uclive.ac.nz>
2015-09-16 18:42:42 +1200

    Comments added to UART main.c and UART text added to deliverable3.txt

--- ESTR_UART/main.c
+++ ESTR_UART/main.c
@@ -9,7 +9,10 @@
  *      				followed by another grave. Checks that
  *      				the expected response "ECHO ON123ECHO OFF"
  *      				is received. Displays a pass/fail on
- *      				Stellaris screen.
+ *      				Stellaris screen or "time out" if no message is received within 1.5 seconds from start-up.  
+ 
+						Furthermore, the program will reset the UUT before testing begins 
+						if pin G4 is connected to the reset pin on the uut ASP connector. 
  *
  */
 
@@ -49,7 +52,6 @@
 #define configUSE_16_BIT_TICKS 0
 
 #define INCLUDE_vTaskDelay 1
-
 #define MIRROR_START_STOP "`"
 #define MIRROR "123"
 #define EXPECTED_RESPONSE "ECHO ON123ECHO OFF"
@@ -64,11 +66,6 @@ void vTimeout( void );
 /* Define the strings that will be passed in as the task parameters.  These are
 defined const and off the stack to ensure they remain valid when the tasks are
 executing. */
-const char *pcTextForTask1 = "Task 1";
-const char *pcTextForTask2 = "Task 2";
-const portTickType LED_delay = 200/portTICK_RATE_MS;
-
-
 
 /*-----------------------------------------------------------*/
 
@@ -76,11 +73,16 @@ const portTickType LED_delay = 200/portTICK_RATE_MS;
 
 int main( void )
 {
+	
+	
+	// ALL INIT calls will be added to an init module at a later date. 
+	
 	/* Set the clocking to run from the PLL at 50 MHz.  Assumes 8MHz XTAL,
 	whereas some older eval boards used 6MHz. */
 	SysCtlClockSet( SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_8MHZ );
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOG);
 	GPIOPinTypeGPIOOutput(GPIO_PORTG_BASE, GPIO_PIN_2);
+	
 	IntMasterEnable();
     // Initialize the OLED display and write status.
     RIT128x96x4Init(1000000);
@@ -101,17 +103,10 @@ int main( void )
 
 	xTaskCreate(	vSend, "Task 2", 240, NULL, 2, NULL );
 	xTaskCreate(	vTimeout, "Task 3", 240, NULL, 2, NULL );
-	//Print task title.
-	//UARTSend((unsigned char *)"Starting\n", 9);
-
-	//Send test message
 
 	//Start the scheduler so the tasks start executing.
 	vTaskStartScheduler();	
 	
-	/* If all is well we will never reach here as the scheduler will now be
-	running.  If we do reach here then it is likely that there was insufficient
-	heap available for the idle task to be created. */
 	for( ;; );
 }
 /*-----------------------------------------------------------*/
@@ -122,7 +117,7 @@ xQueueHandle xToMirror;
 void vTimeout( void )
 {
 
-	stopwatch_t stopwatch;
+	stopwatch_t stopwatch;  // instantiates a stopwatch timer 
 	unsigned long time = 0;
 	char cReceived;
 	int finished = 0;
@@ -134,7 +129,7 @@ void vTimeout( void )
 		{
 			if (xQueueReceive(xToTimeout, &cReceived, (portTickType)10))
 			{
-				//Sending is finished, start timeout timer
+				//Sending is finished, start time out timer
 				if (cReceived == 'S'){
 					break;
 				} else {
@@ -145,7 +140,7 @@ void vTimeout( void )
 		}
 	}
 
-	stopwatch_start(&stopwatch);
+	stopwatch_start(&stopwatch); 
 
 	for( ;; )
 	{
@@ -156,11 +151,11 @@ void vTimeout( void )
 			sprintf(strbuff,"Fail timeout");
 			RIT128x96x4StringDraw(strbuff, 5, 20, 30);
 		}
-		if (xToTimeout !=0)
+		if (xToTimeout !=0)  
 		{
 			if (xQueueReceive(xToTimeout, &cReceived, (portTickType)10))
 			{
-				//Sending is finished, start timeout timer
+				//Sending is finished, start time out timer
 				if (cReceived == 'F'){
 					finished = 1;
 				}
@@ -171,33 +166,30 @@ void vTimeout( void )
 
 void vSend( void )
 {
+	
 	char messageSent = 'S';
-	xToTimeout = xQueueCreate(2, sizeof(char));
+	xToTimeout = xQueueCreate(2, sizeof(char)); // sets up queue to pass message to time out task. 
 	vTaskDelay(500);
 	UARTSend((unsigned char *)MIRROR_START_STOP, sizeof(char));
 	vTaskDelay(200);
 	UARTSend((unsigned char *)MIRROR, MIRROR_LENGTH);
 	vTaskDelay(200);
-	UARTSend((unsigned char *)MIRROR_START_STOP, sizeof(char));
+	UARTSend((unsigned char *)MIRROR_START_STOP, sizeof(char)); // sends message to start time out timer via queue
 
 	xQueueSendToBack( xToTimeout, &messageSent, (portTickType)10);
 
 	for( ;; )
 	{
-		vTaskDelay(1);
+		vTaskDelay(1);    // Present to stop task from ending. 
 	}
 }
 
 void vMirrorTX( void )
-{
 
-	/* The string to print out is passed in via the parameter.  Cast this to a
-	character pointer. */
-	//pcTaskName = ( char * ) pvParameters;
-
-	/* As per most tasks, this task is implemented in an infinite loop. */
+{ 	//  reads mirrored UART characters from UUT and compares them to the charters originally sent to the UUT.
+    //  Magic numbers to be replaced with hash defines in header file. Task also displays results on the stellaris screen
 	char cReceived;
-	char strbuff[20];
+	char strbuff[20]; 
 	int i = 0;
 	char buffer[18] = {0};
 
@@ -212,7 +204,7 @@ void vMirrorTX( void )
 			{
 				buffer[i++] = cReceived;
 				if (i == RESPONSE_LENGTH){
-					if (strncmp(buffer, EXPECTED_RESPONSE, 18)==0){
+					if (strncmp(buffer, EXPECTED_RESPONSE, 18)==0){   // Compares mirrored message to sent message. 
 						sprintf(strbuff,"Pass");
 						RIT128x96x4StringDraw(strbuff, 5, 10, 30);
 						xQueueSendToBack( xToTimeout, &finished, (portTickType)10);
