1ad575db9f8a625353d84059d413f70d8fdce17e
Sam Stephenson <sws52@uclive.ac.nz>
2015-10-04 13:20:31 +1300

    somehow wasnt in the repo

---
+++ ESTR_GPIO/pins.h
@@ -0,0 +1,21 @@
+/*
+ * pins.h
+ *
+ *  Created on: Sep 13, 2015
+ *      Author: sws52
+ */
+
+#ifndef PINS_H_
+#define PINS_H_
+
+#define STATUS_LED_PG2 (1<<2)
+
+
+//#define UUT_AIRSPEED_OUTPUT_PIN_PE2 (1<<2)
+//#define UUT_TRANSPONDER_OUTPUT_PIN_PE0 (1<<0)
+
+#define UUT_TRANSPONDER_RESPONSE_PIN_PB3 (1<<3)
+#define UUT_AIRSPEED_RESPONSE_PIN_PE3 (1<<3)
+
+
+#endif /* PINS_H_ */
